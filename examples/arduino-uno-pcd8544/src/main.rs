d9afb5b13bf798fd68d3baca1d79d6125c699ade
Nabil Elqatib <nabilelqatib@gmail.com>
2025-05-07 00:01:25 +0200

    Create an example to display Rust logo using an Arduino board

---
+++ examples/arduino-uno-pcd8544/src/main.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use pcd8544_hal::Pcd8544;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    let mut pcd8544 = pcd8544_hal::Pcd8544Gpio::new(
+        /* sck/clk */ pins.d7.into_output(),
+        /* miso/din */ pins.d6.into_output(),
+        /* mosi/dc */ pins.d5.into_output(),
+        /* cs/ce */ pins.d4.into_output(),
+        /* rst */ &mut pins.d3.into_output(),
+        &mut arduino_hal::Delay::new(),
+    );
+    pcd8544_hal::demo::demo(&mut pcd8544);
+
+    #[allow(clippy::empty_loop)]
+    loop {}
+}
